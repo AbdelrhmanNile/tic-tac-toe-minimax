dab18d73934898e021610d944af08359ca0be7b8
AbdelrhmanNile <abdelrhmannile@gmail.com>
2022-05-26 10:45:43 +0200

    added gui

---
+++ gui.py
@@ -0,0 +1,264 @@
+from tkinter import *
+from tkinter import messagebox
+import xo
+
+
+root = Tk()
+# root.title('Codemy.com - Tic-Tac-Toe')
+# root.iconbitmap('c:/gui/codemy.ico')
+# root.geometry("1200x710")
+
+# X starts so true
+clicked = True
+count = 0
+
+
+# disable all the buttons
+def disable_all_buttons():
+    b1.config(state=DISABLED)
+    b2.config(state=DISABLED)
+    b3.config(state=DISABLED)
+    b4.config(state=DISABLED)
+    b5.config(state=DISABLED)
+    b6.config(state=DISABLED)
+    b7.config(state=DISABLED)
+    b8.config(state=DISABLED)
+    b9.config(state=DISABLED)
+
+
+# Check to see if someone won
+def checkifwon():
+    global winner
+    winner = False
+
+    if xo.checkWin()[0] == True:
+        indxs = xo.checkWin()[2]
+        print(indxs)
+        for i in range(3):
+            btn[indxs[i]].config(bg="red")
+
+        winner = True
+        if xo.checkWin()[1] == "X":
+            messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS!  X Wins!!")
+        else:
+            messagebox.showinfo("Tic Tac Toe", "CONGRATULATIONS!  O Wins!!")
+        disable_all_buttons()
+        return True
+
+    # Check if tie
+    if xo.checkDraw():
+        messagebox.showinfo("Tic Tac Toe", "It's A Tie!\n No One Wins!")
+        disable_all_buttons()
+        return False
+
+
+# Button clicked function
+def b_click(b, i):
+    global clicked, count
+
+    if b["text"] == " " and clicked == True:
+        playerMoveGUI(i)
+        clicked = False
+        count += 1
+        if checkifwon():
+            exit()
+        else:
+            aiMoveGui()
+            checkifwon()
+    elif b["text"] == " " and clicked == False:
+        playerMoveGUI(i)
+        clicked = True
+        count += 1
+        if checkifwon():
+            exit()
+        else:
+            aiMoveGui()
+            checkifwon()
+    else:
+        messagebox.showerror(
+            "Tic Tac Toe",
+            "Hey! That box has already been selected\nPick Another Box...",
+        )
+
+
+def startClick(btn0):
+    aiMoveGui()
+    btn0.config(state=DISABLED)
+    clicked = False
+    global count
+    count += 1
+
+
+def getPos(index):
+    return xo.board[index]
+
+
+# Start the game over!
+def reset():
+    global b1, b2, b3, b4, b5, b6, b7, b8, b9, b0
+    global clicked, count
+    clicked = True
+    count = 0
+
+    # Build our buttons
+    b1 = Button(
+        root,
+        text=getPos(1),
+        font=("Helvetica", 20),
+        height=3,
+        width=6,
+        bg="black",
+        command=lambda i=1: b_click(b1, i),
+        name="1",
+    )
+    b2 = Button(
+        root,
+        text=getPos(2),
+        font=("Helvetica", 20),
+        height=3,
+        width=6,
+        bg="black",
+        command=lambda i=2: b_click(b2, i),
+        name="2",
+    )
+    b3 = Button(
+        root,
+        text=getPos(3),
+        font=("Helvetica", 20),
+        height=3,
+        width=6,
+        bg="black",
+        command=lambda i=3: b_click(b3, i),
+        name="3",
+    )
+
+    b4 = Button(
+        root,
+        text=getPos(4),
+        font=("Helvetica", 20),
+        height=3,
+        width=6,
+        bg="black",
+        command=lambda i=4: b_click(b4, i),
+        name="4",
+    )
+    b5 = Button(
+        root,
+        text=getPos(5),
+        font=("Helvetica", 20),
+        height=3,
+        width=6,
+        bg="black",
+        command=lambda i=5: b_click(b5, i),
+        name="5",
+    )
+    b6 = Button(
+        root,
+        text=getPos(6),
+        font=("Helvetica", 20),
+        height=3,
+        width=6,
+        bg="black",
+        command=lambda i=6: b_click(b6, i),
+        name="6",
+    )
+
+    b7 = Button(
+        root,
+        text=getPos(7),
+        font=("Helvetica", 20),
+        height=3,
+        width=6,
+        bg="black",
+        command=lambda i=7: b_click(b7, i),
+        name="7",
+    )
+    b8 = Button(
+        root,
+        text=getPos(8),
+        font=("Helvetica", 20),
+        height=3,
+        width=6,
+        bg="black",
+        command=lambda i=8: b_click(b8, i),
+        name="8",
+    )
+    b9 = Button(
+        root,
+        text=getPos(9),
+        font=("Helvetica", 20),
+        height=3,
+        width=6,
+        bg="black",
+        command=lambda i=9: b_click(b9, i),
+        name="9",
+    )
+
+    b0 = Button(
+        root,
+        text="start",
+        font=("Helvetica", 20),
+        height=3,
+        width=6,
+        bg="white",
+        command=lambda i=0: startClick(b0),
+    )
+
+    global btn
+    btn = {1: b1, 2: b2, 3: b3, 4: b4, 5: b5, 6: b6, 7: b7, 8: b8, 9: b9}
+
+    # Grid our buttons to the screen
+    b1.grid(row=1, column=0)
+    b2.grid(row=1, column=1)
+    b3.grid(row=1, column=2)
+
+    b4.grid(row=2, column=0)
+    b5.grid(row=2, column=1)
+    b6.grid(row=2, column=2)
+
+    b7.grid(row=3, column=0)
+    b8.grid(row=3, column=1)
+    b9.grid(row=3, column=2)
+    b0.grid(row=0, column=1)
+
+
+def redIt(i1, i2, i3):
+    btn[i1].config(bg="red")
+    btn[i2].config(bg="red")
+    btn[i3].config(bg="red")
+
+
+def updatePos():
+    b1["text"] = getPos(1)
+    b2["text"] = getPos(2)
+    b3["text"] = getPos(3)
+    b4["text"] = getPos(4)
+    b5["text"] = getPos(5)
+    b6["text"] = getPos(6)
+    b7["text"] = getPos(7)
+    b8["text"] = getPos(8)
+    b9["text"] = getPos(9)
+
+
+def aiMoveGui():
+    xo.aiMove()
+    updatePos()
+
+
+def playerMoveGUI(i):
+    xo.playerMove(int(i))
+    updatePos()
+
+
+# Create menue
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+# Create Options Menu
+options_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="Options", menu=options_menu)
+options_menu.add_command(label="Rest Game", command=reset)
+
+reset()
+
+root.mainloop()
--- xo.py
+++ xo.py
@@ -1,81 +1,83 @@
 import os
-
+from random import randint
 # board structure is made with a dict, index from 1 to 9 indicating positions on the board
 # all positions are intialized empty
-board = {1:' ', 2:' ', 3:' ',
-         4:' ', 5:' ', 6:' ',
-         7:' ', 8:' ', 9:' '}
+board = {1: " ", 2: " ", 3: " ", 4: " ", 5: " ", 6: " ", 7: " ", 8: " ", 9: " "}
 
 # ai playes with X "maximizing"
 # player playes with O "minimizing"
-ai = 'X'
-player = 'O'
+ai = "X"
+player = "O"
 
 # function to draw the board
 def drawBoard(board):
     for key in board:
         if key % 3 == 0:
-            print(board[key] + '\n')
+            print(board[key] + "\n")
         else:
-            print(board[key] + ' | ', end='')
+            print(board[key] + " | ", end="")
+
 
 # functions to check if a position is empty or no
 def isEmpty(pos):
-    if board[pos] == ' ':
+    if board[pos] == " ":
         return True
     else:
         return False
 
+
 # function to check if it is a draw
 def checkDraw():
     for key in board:
-        if board[key] == ' ':
+        if board[key] == " ":
             return False
     return True
+
 
 # function to check for win, it returns a dict i.e. {0: True, 1: 'X'}
 # which means there is a win and 'X' is the winner
 # index 0 True or False (win or no win), index 1 winner ('X' or 'O' or None)
 def checkWin():
-    if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
-        return {0:True, 1:board[1]}
-    elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
-        return {0:True, 1:board[4]}
-    elif (board[7] == board[8] and board[7] == board[9] and board[7] != ' '):
-        return {0:True, 1:board[7]}
-    elif (board[1] == board[4] and board[1] == board[7] and board[1] != ' '):
-        return {0:True, 1:board[1]}
-    elif (board[2] == board[5] and board[2] == board[8] and board[2] != ' '):
-        return {0:True, 1:board[2]}
-    elif (board[3] == board[6] and board[3] == board[9] and board[3] != ' '):
-        return {0:True, 1:board[3]}
-    elif (board[1] == board[5] and board[1] == board[9] and board[1] != ' '):
-        return {0:True, 1:board[1]}
-    elif (board[7] == board[5] and board[7] == board[3] and board[7] != ' '):
-        return (True, board[7])
+    if board[1] == board[2] and board[1] == board[3] and board[1] != " ":
+        return {0: True, 1: board[1], 2: [1,2,3]}
+    elif board[4] == board[5] and board[4] == board[6] and board[4] != " ":
+        return {0: True, 1: board[4], 2: [4,5,6]}
+    elif board[7] == board[8] and board[7] == board[9] and board[7] != " ":
+        return {0: True, 1: board[7], 2: [7,8,9]}
+    elif board[1] == board[4] and board[1] == board[7] and board[1] != " ":
+        return {0: True, 1: board[1], 2: [1,4,7]}
+    elif board[2] == board[5] and board[2] == board[8] and board[2] != " ":
+        return {0: True, 1: board[2], 2: [2,5,8]}
+    elif board[3] == board[6] and board[3] == board[9] and board[3] != " ":
+        return {0: True, 1: board[3], 2: [3,6,9]}
+    elif board[1] == board[5] and board[1] == board[9] and board[1] != " ":
+        return {0: True, 1: board[1], 2: [1,5,9]}
+    elif board[7] == board[5] and board[7] == board[3] and board[7] != " ":
+        return {0: True, 1: board[7], 2: [7,5,3]}
     else:
-        return {0:False, 1:None}
+        return {0: False, 1: None, 2: None}
+
 
 # insert a letter 'X' or 'O' at position
 def insertPos(letter, pos):
     if isEmpty(pos):
-        board[pos] = letter;
-        os.system('clear')
+        board[pos] = letter
+        os.system("clear")
         drawBoard(board)
 
-        #check for win after inserting
-        if(checkWin()[0]):
-            if letter == 'X':
+        # check for win after inserting
+        if checkWin()[0]:
+            if checkWin()[1] == "X":
                 print("Ai wins")
-                exit()
+                # exit()
             else:
                 print("player wins")
-                exit()
-        if(checkDraw()):
+                # exit()
+        if checkDraw():
             print("IT IS DRAW!")
-            exit()
+            # exit()
         return
-    else: # if the provided position is not empty, re enter position
+    else:  # if the provided position is not empty, re enter position
         print("position is not empty")
         pos = int(input("enter another position: "))
         insertPos(letter, pos)
@@ -83,22 +85,22 @@ def insertPos(letter, pos):
 
 
 def minimax(board, depth, isMaximizing):
-    if(checkWin()[1] == ai):
+    if checkWin()[1] == ai:
         return 100
-    elif(checkWin()[1] == player):
+    elif checkWin()[1] == player:
         return -100
-    elif(checkDraw()):
+    elif checkDraw():
         return 0
 
-    if(isMaximizing):
+    if isMaximizing:
         bestScore = -1000
 
         for key in board:
-            if(board[key]==' '):
+            if board[key] == " ":
                 board[key] = ai
                 score = int(minimax(board, 100, False))
-                board[key] = ' '
-                if(score > bestScore):
+                board[key] = " "
+                if score > bestScore:
                     bestScore = score
         return bestScore
 
@@ -106,37 +108,40 @@ def minimax(board, depth, isMaximizing):
         bestScore = 1000
 
         for key in board:
-            if(board[key]==' '):
+            if board[key] == " ":
                 board[key] = ai
                 score = int(minimax(board, 100, True))
-                board[key] = ' '
-                if(score < bestScore):
+                board[key] = " "
+                if score < bestScore:
                     bestScore = score
         return bestScore
 
 
-
-def playerMove():
-    pos = int(input("enter position: "))
+def playerMove(pos):
     insertPos(player, pos)
     return
+def getrand():
+    return randint(1,9)    
+
 
 def aiMove():
     bestScore = -1000
     bestMove = 0
 
     for key in board:
-        if(board[key]==' '):
+        if board[key] == " ":
             board[key] = ai
             score = int(minimax(board, 100, False))
-            board[key] = ' '
-            if(score > bestScore):
+            board[key] = " "
+            if score > bestScore:
                 bestScore = score
                 bestMove = key
 
     insertPos(ai, bestMove)
-    return
+    return bestMove
 
-while not checkWin()[0]:
-    aiMove()
-    playerMove()
+
+def start_game():
+    while not checkWin()[0]:
+        aiMove()
+        playerMove()
